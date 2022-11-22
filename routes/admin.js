aee8d0f855794fc705c69498fcc6e71a3fa9fa0a
Gilzon <gilzon7@gmail.com>
2022-11-22 22:25:30 +0530

    Products added From Database by Admin

--- routes/admin.js
+++ routes/admin.js
@@ -5,34 +5,12 @@ const productHelpers = require('../helpers/product-helpers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let products=[
-    {
-       name:"Hoodie",
-       category:"hoodies",
-       description:"Premium Printed Hoodies",
-       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTezSut3Aq2i7khS5NbWqpwGl9zfovuRd_yA&usqp=CAU"
-    },
-    {
-      name:"Hoodie",
-      category:"hoodies",
-      description:"Premium Printed Hoodies",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4HVt-9lOAhibrlhXTNgTXxaF9BZpZWEj4yg&usqp=CAU"
-   },
-   {
-    name:"Hoodie",
-    category:"hoodies",
-    description:"Premium Printed Hoodies",
-  image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI1aeKfrIF3k60vnrHG8C86UKvHNAXvplavk_O1bU1Tzuxo77dr802jPkIXwrHlf2PbkQ&usqp=CAU"
- },
- {
-  name:"Hoodie",
-  category:"hoodies",
-  description:"Premium Printed Hoodies",
-  image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNEJnvGKcGg3qyxfsacAU2CmczyKfkH0uia0K1B3A53E8nIhkavRR-AIHOyJH3Qo5rJqA&usqp=CAU"
-},
-  ]
-  
+ productHelpers.getAllProducts().then((products)=>{
+  console.log(products);
   res.render('admin/view-products', {admin:true,products});
+ })
+  
+ 
 });
 router.get('/add-product',function(req,res){
 res.render('admin/add-product')
--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const productHelpers = require('../helpers/product-helpers');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let products=[
-    {
-       name:"Hoodie",
-       category:"hoodies",
-       description:"Premium Printed Hoodies",
-       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTezSut3Aq2i7khS5NbWqpwGl9zfovuRd_yA&usqp=CAU"
-    },
-    {
-      name:"Hoodie",
-      category:"hoodies",
-      description:"Premium Printed Hoodies",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4HVt-9lOAhibrlhXTNgTXxaF9BZpZWEj4yg&usqp=CAU"
-   },
-   {
-    name:"Hoodie",
-    category:"hoodies",
-    description:"Premium Printed Hoodies",
-  image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI1aeKfrIF3k60vnrHG8C86UKvHNAXvplavk_O1bU1Tzuxo77dr802jPkIXwrHlf2PbkQ&usqp=CAU"
- },
- {
-  name:"Hoodie",
-  category:"hoodies",
-  description:"Premium Printed Hoodies",
-  image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNEJnvGKcGg3qyxfsacAU2CmczyKfkH0uia0K1B3A53E8nIhkavRR-AIHOyJH3Qo5rJqA&usqp=CAU"
-},
-  ]
-  res.render('index', {products});
+ 
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('user/view-products', {admin:false,products});
+   })
+
+ 
   
 });
 
