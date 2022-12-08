4aaebb06703d544edf0a7425839b4df1ad727687
Gilzon <gilzon7@gmail.com>
2022-12-08 15:08:21 +0530

    Edit Product

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const e = require('express');
 const { response } = require('express');
 var express = require('express');
+const res = require('express/lib/response');
 var router = express.Router();
 
 const productHelpers = require('../helpers/product-helpers');
@@ -18,8 +19,8 @@ router.get('/add-product', function (req, res) {
   res.render('admin/add-product')
 })
 router.post('/add-product', (req, res) => {
-  console.log(req.body);
-  console.log(req.files.Image);
+ // console.log(req.body);
+  //console.log(req.files.Image);
 
   productHelpers.addProduct(req.body, (id) => {
     let image = req.files.Image
@@ -37,10 +38,30 @@ router.post('/add-product', (req, res) => {
 
 router.get('/delete-product/:id', (req, res) => {
   let proId = req.params.id
-  console.log(proId);
- 
+
+
   productHelpers.deleteProduct(proId).then((response) => {
     res.redirect('/admin/')
   })
+})
+
+router.get('/edit-product/:id',async(req,res)=>{
+  let product=await productHelpers.getProductDetails(req.params.id)
+  console.log(product)
+  res.render("admin/edit-product",{product})
+})
+
+router.post('/edit-product/:id',(req,res)=>{
+  console.log(req.params.id);
+  let id=req.params.id
+productHelpers.updateProduct(req.params.id,req.body).then(()=>{
+  res.redirect('/admin')
+  if(req.files.Image){
+    let image=req.files.Image
+
+    image.mv('./public/product-images/' + id + '.jpg')
+
+  }
+})
 })
 module.exports = router;
