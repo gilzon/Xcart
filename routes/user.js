8b1be0125b66f7d792ba90cf4d92a4cd1bb6a5c1
Gilzon <gilzon7@gmail.com>
2022-12-15 11:49:36 +0530

    Managed cart products and quantity

--- routes/user.js
+++ routes/user.js
@@ -22,11 +22,13 @@ router.get('/', async function (req, res, next) {
   // console.log(user);
   let cartCount = null
   if (req.session.user) {
+   
    cartCount= await userHelpers.getcartCount(req.session.user._id)
   }
   productHelpers.getAllProducts().then((products) => {
 
     res.render('user/view-products', { products, user, cartCount });
+   
 
   })
 
@@ -77,7 +79,7 @@ router.get('/logout', (req, res) => {
 })
 router.get('/cart', verifyLogin, async (req, res) => {
   let products = await userHelpers.getCartProducts(req.session.user._id)
-  console.log(products,'product list')
+  console.log(products)
   res.render('user/cart', { products, user: req.session.user })
 
 })
