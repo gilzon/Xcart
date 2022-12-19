d1f83f7ac1650693762e95ca339b9d7b8524f960
Gilzon <gilzon7@gmail.com>
2022-12-19 15:46:43 +0530

    Product quantity in ajax , price added in cart and view products

--- routes/user.js
+++ routes/user.js
@@ -1,101 +1,92 @@
-const { response } = require('express');
-var express = require('express');
-const res = require('express/lib/response');
+const { response } = require("express");
+var express = require("express");
+const res = require("express/lib/response");
 var router = express.Router();
-const productHelpers = require('../helpers/product-helpers');
-const userHelpers = require('../helpers/user-helpers');
+const productHelpers = require("../helpers/product-helpers");
+const userHelpers = require("../helpers/user-helpers");
 const ObjectId = require("mongodb").ObjectId;
 
 const verifyLogin = (req, res, next) => {
   if (req.session.loggedIn) {
-    next()
+    next();
   } else {
-    res.redirect('/login')
+    res.redirect("/login");
   }
-}
-
-
+};
 
 /* GET users listing. */
-router.get('/', async function (req, res, next) {
-  let user = req.session.user
+router.get("/", async function (req, res, next) {
+  let user = req.session.user;
   // console.log(user);
-  let cartCount = null
+  let cartCount = null;
   if (req.session.user) {
-   
-   cartCount= await userHelpers.getcartCount(req.session.user._id)
+    cartCount = await userHelpers.getcartCount(req.session.user._id);
   }
   productHelpers.getAllProducts().then((products) => {
-
-    res.render('user/view-products', { products, user, cartCount });
-   
-
-  })
-
+    res.render("user/view-products", { products, user, cartCount });
+  });
 });
 
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/')
+    res.redirect("/");
   } else {
-    res.render('user/login', { "logginErr": req.session.loginErr })
-    req.session.logginErr = false, { title: 'LOGIN' }
+    res.render("user/login", { logginErr: req.session.loginErr });
+    (req.session.logginErr = false), { title: "LOGIN" };
   }
 });
 
-
-router.get('/signup', (req, res) => {
-  res.render('user/signup'), { title: 'signup' }
+router.get("/signup", (req, res) => {
+  res.render("user/signup"), { title: "signup" };
 });
 
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   userHelpers.doSignup(req.body).then((response) => {
     console.log(response);
-    req.session.loggedIn = true
-    req.session.user = response
-    res.redirect('/')
-  })
-})
+    req.session.loggedIn = true;
+    req.session.user = response;
+    res.redirect("/");
+  });
+});
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   userHelpers.doLogin(req.body).then((response) => {
     if (response.status) {
-      req.session.loggedIn = true
-      req.session.user = response.user
-      res.redirect('/')
-
+      req.session.loggedIn = true;
+      req.session.user = response.user;
+      res.redirect("/");
     } else {
-      req.session.loginErr = "Invalid Username or Password"
-      res.redirect('/login')
+      req.session.loginErr = "Invalid Username or Password";
+      res.redirect("/login");
     }
-  })
-})
+  });
+});
 
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
+router.get("/cart", verifyLogin, async (req, res) => {
+  let products = await userHelpers.getCartProducts(req.session.user._id);
+  console.log(products);
+  res.render("user/cart", { products, user: req.session.user });
+});
 
-router.get('/logout', (req, res) => {
-  req.session.destroy()
-  res.redirect('/login')
-})
-router.get('/cart', verifyLogin, async (req, res) => {
-  let products = await userHelpers.getCartProducts(req.session.user._id)
-  console.log(products)
-  res.render('user/cart', { products, user: req.session.user })
-
-})
-
-router.get('/add-to-cart/:id',  (req, res) => {
+router.get("/add-to-cart/:id", (req, res) => {
   console.log("api call");
   userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
-    res.json({status:true})
-  })
-})
+    res.json({ status: true });
+  });
+});
 
-router.post('/change-product-quantity',(req,res,next)=>{
-  
-  userHelpers.changeProductQuantity(req.body).then(()=>{
+router.post("/change-product-quantity", (req, res, next) => {
+  userHelpers.changeProductQuantity(req.body).then((response) => {
+    res.json(response);
+  });
+});
+// userHelpers.removeProduct(proId).then((response) => {
+//   res.redirect('/cart')
+// })
 
-  })
-  })
 
 module.exports = router;
